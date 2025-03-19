0cb24042cc6f05eb4847f6f6b895e79f508d27af
Oleh Parshenko <jupiterblitzpower@gmail.com>
2025-03-19 08:33:20 +0200

    Code improvements del validation in CompanyInDto.

--- Jobs.CompanyApi/DTOModels/CompanyInDto.cs
+++ Jobs.CompanyApi/DTOModels/CompanyInDto.cs
@@ -1,15 +1,11 @@
-using Jobs.CompanyApi.Validators;
-
 namespace Jobs.CompanyApi.DTOModels;
 
-public record CompanyInDto( int CompanyId, 
-                            string CompanyName, 
+public record CompanyInDto( int CompanyId,
+                            string CompanyName,
                             string CompanyDescription,
                             string CompanyLogoPath,
                             string CompanyLink,
                             bool IsVisible = true,
-                            bool IsActive = true) 
-{
-    public bool IsValid() => new CompanyInDtoValidator().Validate(this).IsValid;
-}
+                            bool IsActive = true );
+
 
